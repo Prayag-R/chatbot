70c7ec27fca518897cca66a2230ae63bf062d0d6
Prayag-R <prayag.s.rana@gmail.com>
2025-11-03 17:07:23 -0500

    demov8

--- public/widget.js
+++ public/widget.js
@@ -10,13 +10,21 @@
   iframe.style.right = "20px";
   iframe.style.width = "70px";
   iframe.style.height = "70px";
-  iframe.style.border = "none";
+  iframe.style.border = "0";
   iframe.style.borderRadius = "50%";
   iframe.style.overflow = "hidden";
   iframe.style.zIndex = "9999";
   iframe.style.background = "transparent";
-  iframe.style.boxShadow = "0 4px 12px rgba(0,0,0,0.2)";
+  iframe.style.boxSizing = "border-box";
+  iframe.style.padding = "0";
+  iframe.style.margin = "0";
+  iframe.style.pointerEvents = "auto";
   iframe.style.transition = "all 0.3s ease-in-out";
+  
+  // Remove any possible visual artifacts
+  iframe.style.webkitMaskImage = "-webkit-radial-gradient(white, black)";
+  iframe.style.outline = "none";
+  iframe.style.boxShadow = "none";
 
   document.body.appendChild(iframe);
 
@@ -25,11 +33,12 @@
   // expand / collapse on click
   iframe.addEventListener("click", () => {
     if (!expanded) {
-      iframe.style.width = "420px";
+      iframe.style.width = "380px";
       iframe.style.height = "600px";
-      iframe.style.borderRadius = "20px";
+      iframe.style.borderRadius = "12px";
       iframe.style.bottom = "20px";
       iframe.style.right = "20px";
+      iframe.style.background = "transparent";
       expanded = true;
     } else {
       iframe.style.width = "70px";
